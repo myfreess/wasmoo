983bcacd1ea88b78ed9bf88189ae950a57dc0a57
myfreess <neomyfreess@outlook.com>
2025-09-25 15:51:35 +0800

    save

--- install.ts
+++ install.ts
@@ -1,4 +1,7 @@
-const download_toolchain_tarball = async (url: string, dest: string) => {
+
+const tarball_url = `https://github.com/moonbitlang/moonbit-compiler/releases/latest/download/moonbit-wasm.tar.gz`;
+
+
+const download_toolchain_tarball = async () => {
     // curl -fSL -O https://github.com/moonbitlang/moonbit-compiler/releases/latest/download/moonbit-wasm.tar.gz
-    
 }
